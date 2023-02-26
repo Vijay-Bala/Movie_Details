4222aa3584697925bc9bcca695e01885964fbd54
Vijay-Bala <95472564+Vijay-Bala@users.noreply.github.com>
2023-02-26 17:34:26 +0530

    Update Movie2.py

--- Movie2.py
+++ Movie2.py
@@ -1,31 +1,21 @@
 import requests
 
 def get_movie_details(title):
-    # replace "YOUR_API_KEY" with your actual OMDb API key
-    url = f"http://www.omdbapi.com/?apikey=27e36d7d&t={title}&plot=full&r=json"
-
-    # send a GET request to the API
-    response = requests.get(url)
-
-    # check if the request was successful
-    if response.status_code == 200:
+    #I have used OMDb API for my project.You can use other APIs also which has the data about the movies
+    url = f"http://www.omdbapi.com/?apikey=27e36d7d&t={title}&plot=full&r=json"          # replace this apikey with your actual OMDb API key
+    response = requests.get(url)                                                         # sending a GET request to the API
+    if response.status_code == 200:                                                      # checking whether the request was successful or not
         data = response.json()
-
-        # check if the API returned a valid response
-        if data["Response"] == "True":
+        if data["Response"] == "True":                                                   # checking whether the API returned is a valid response or not
             title = data["Title"]
             director = data["Director"]
             cast = data["Actors"]
             soundtrack = data.get("Soundtrack", "N/A")
-
-            # return the details as a string
-            return f"Title: {title}\nDirector: {director}\nCast: {cast}\nSoundtrack: {soundtrack}"
+            return f"Title: {title}\nDirector: {director}\nCast: {cast}\nSoundtrack: {soundtrack}"             # returns the details as string
         else:
             return f"Sorry, could not find details for {title}"
     else:
         return "Sorry, could not connect to the API."
-
-# test the function
 title = input("Please enter the movie title: ")
 details = get_movie_details(title)
 print(details)
